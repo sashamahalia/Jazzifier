92b2388b18d52e73e9dc4791a233f2f9795c698a
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-05-06 15:14:22 -0700

    Display play button

---
+++ src/components/PlayButton.vue
@@ -0,0 +1,17 @@
+import * as Tone from 'tone'
+
+<template>
+  <button :disabled="!isLoaded" @click="handleClick">
+      Play
+    </button>
+</template>
+
+<script>
+export default {
+  name: 'PlayButton'
+}
+</script>
+
+<style lang="scss">
+  
+</style>
