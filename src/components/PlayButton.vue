95f4f4cce8f0e5e0b2ca65720f317670459f5380
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-05-06 18:01:33 -0700

    Implement loop and start/stop functionality. Play back has a bug with audio phasing

--- src/components/PlayButton.vue
+++ src/components/PlayButton.vue
@@ -8,6 +8,7 @@
 import * as Tone from 'tone'
   export default {
   name: 'PlayButton',
+  //where state lives
   data() {
     return {
       play: 'Play'
@@ -16,14 +17,27 @@ import * as Tone from 'tone'
   methods: {
     handleClick() {
       const synth = new Tone.PolySynth(Tone.Synth).toDestination();
-      this.switchPlay()
-      synth.triggerAttackRelease(["C4", "f4", "B4", "D4"], 2);
+      this.switchPlay();
+      // eslint-disable-next-line no-unused-vars
+      const loop = new Tone.Loop(time => {
+        synth.triggerAttackRelease("C2", "8n", time);
+      }, "4n").start(0);
+
+      Tone.Transport.bpm.value = 60
     },
     switchPlay() {
       if (this.play !== 'Play') {
+        this.stop()
         return this.play = 'Play'
       }
+      this.start()
       return this.play = 'Stop'
+    },
+    stop() {
+      Tone.Transport.stop()
+    },
+    start() {
+      Tone.Transport.start();
     }
   }
 }
