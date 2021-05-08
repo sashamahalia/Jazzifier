6019d60cffcf7a2e81a095f251aafb16fb7c0ef9
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-05-08 11:53:51 -0700

    refactor into modular helper functions

---
+++ src/helpers/chords.js
@@ -0,0 +1,6 @@
+export const chords = {
+  chord1: ["C3", "E4", "G4", "A4"],
+  chord2: ["D3", "F4", "A4", "B4"],
+  chord3: ["E3", "G4", "B4", "C4"],
+  chord4: ["F3", "A4", "C4", "E4"]
+};
--- src/helpers/loop.js
+++ src/helpers/loop.js
@@ -1,21 +1,12 @@
 import * as Tone from 'tone';
+import { synth } from './synth'
+import { chords } from './chords'
+
 import s11 from 'sharp11';
 
-export let synth = new Tone.PolySynth(Tone.Synth, {
-  oscillator : {
-    type : "sawtooth",
-    detune: 3,
-    portamento: 0.5
-  }
-}).toDestination();
-
-export const chords = {
-  chord1: ["C3", "E4", "G4", "A4"],
-  chord2: ["D3", "F4", "A4", "B4"],
-  chord3: ["E3", "G4", "B4", "C4"],
-  chord4: ["F3", "A4", "C4", "E4"]
-};
-
+//using synth and chords from helper functions
+synth;
+chords;
 
 //mainChords syntax inspired by https://www.devbridge.com/articles/tonejs-coding-music-production-guide/
 const mainChords = [
@@ -28,6 +19,7 @@ const mainChords = [
 export const chordLoop = new Tone.Part((time, value) => {
   //value.note is the array of notes in the chord, s11.identify analyzes what chord it is and returns the chord name as a string
     document.getElementById("chord-name").innerHTML = s11.chord.identify(...value.note);
+    //testing how to conditionally display based on each beat
     if (value.note === chords.chord1) { console.log(1) }
     synth.triggerAttackRelease(value.note, value.duration, time);
 }, mainChords).start(0);
@@ -36,8 +28,4 @@ export const chordLoop = new Tone.Part((time, value) => {
 chordLoop.loopStart = 0;
 chordLoop.loopEnd = "4m";
 chordLoop.loop = true;
-
-export const bpm = num => {
-  return Tone.Transport.bpm.value = num;
-}
  
