ee6dba10866b5f14f67870843e1b7e518f243fc1
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-05-09 11:27:17 -0700

    Current chord changes state in play button component, displaying within button text for demo purposes

--- src/helpers/loop.js
+++ src/helpers/loop.js
@@ -1,8 +1,7 @@
+import Vue from "vue"
 import * as Tone from 'tone';
 import { synth } from './synth'
 import { chords } from './chords'
-import playButton from '../components/PlayButton.vue'
-
 import s11 from 'sharp11';
 
 //using synth and chords from helper functions
@@ -17,25 +16,24 @@ const mainChords = [
   {'time': '3:0', 'note': chords.chord4, 'duration': '1m'},
 ];
 
+//this makes a one-off state store for sharing between files
+export const chordState = new Vue.observable({chord: ''})
+
 export const chordLoop = new Tone.Part((time, value) => {
-  //value.note is the array of notes in the chord, s11.identify analyzes what chord it is and returns the chord name as a string
-    document.getElementById("chord-name").innerHTML = s11.chord.identify(...value.note);
-    //testing how to conditionally display based on each beat
+    //value.note is the array of notes in the chord, s11.identify analyzes what chord it is and returns the chord name as a string
+    //conditionally renders chord name as state
     if (value.note === chords.chord1){
-      playButton.data.chord = s11.chord.identify(...value.note)
-      console.log(playButton.data.chord)
+      chordState.chord = s11.chord.identify(...value.note)
+      console.log(chordState.chord)
     }
     if (value.note === chords.chord2){
-      playButton.data.chord = s11.chord.identify(...value.note)
-      console.log(playButton.data.chord)
+      chordState.chord = s11.chord.identify(...value.note)
     }
     if (value.note === chords.chord3){
-      playButton.data.chord = s11.chord.identify(...value.note)
-      console.log(playButton.data.chord)
+      chordState.chord = s11.chord.identify(...value.note)
     }
     if (value.note === chords.chord4){
-      playButton.data.chord = s11.chord.identify(...value.note)
-      console.log(playButton.data.chord)
+      chordState.chord = s11.chord.identify(...value.note)
     }
 
     synth.triggerAttackRelease(value.note, value.duration, time);
