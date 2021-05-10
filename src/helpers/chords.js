ed4a04bfa52f332e14c962d4d1774def80e560f8
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-05-10 08:16:01 -0700

    Parse chord names into array of chord notes, feed to Tone as input

--- src/helpers/chords.js
+++ src/helpers/chords.js
@@ -1,6 +1,20 @@
+import s11 from 'sharp11';
+
+const selectChords = ['Emaj7#11', 'Gm6', 'F/C', 'C'];
+
+const chordArray = selectChords.map(chord => s11.chord.create(chord));
+console.log(chordArray);
+
+//chordArray is an array of Sharp 11 chord objects which have bunch of info
+//including the letter names of each note in the chord which is what we're returning
+//Added a '3' to make the notes readable by Tone, but it's not a perfect solution and sometime leads to weird results
+const chordNotes = chordArray.map(chordInfo => chordInfo.chord.map(note => {
+  return `${note.letter}3`
+}));
+
 export const chords = {
-  chord1: ["C3", "E4", "G4", "A4"],
-  chord2: ["D3", "F4", "A4", "B4"],
-  chord3: ["E3", "G4", "B4", "C4"],
-  chord4: ["F3", "A4", "C4", "E4"]
+  chord1: chordNotes[0],
+  chord2: chordNotes[1],
+  chord3: chordNotes[2],
+  chord4: chordNotes[3]
 };
