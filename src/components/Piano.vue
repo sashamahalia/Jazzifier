a5ace3ba42f0a7a9cb04453d6cbd044811b236fd
Stephanie <stephanzonator@hotmail.com>
2021-05-10 12:02:13 -0700

    add prototype css piano key

--- src/components/Piano.vue
+++ src/components/Piano.vue
@@ -1,14 +1,31 @@
 <template lang="">
   <div>
     <p>Piano goes here potato</p>
+    <div class="keys">
+      <LeftKey />
+      <LeftKey />
+      <LeftKey />
+      <LeftKey />
+      <LeftKey />
+    </div>
   </div>
 </template>
 <script>
+import LeftKey from './pianokeys/LeftKey';
 export default {
-  name: 'Piano'
+  name: 'Piano',
+  components: {
+    LeftKey
+  }
   
 }
 </script>
-<style lang="">
+<style lang="scss">
+.keys {
+  // width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: nowrap;
+}
   
 </style>
---
+++ src/components/pianokeys/LeftKey.vue
@@ -0,0 +1,19 @@
+<template lang="">
+  <div class="key">
+    <span></span>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LeftKey'
+  
+}
+</script>
+<style lang="scss">
+.key {
+  padding: 2em;
+  background-color: orange;
+  clip-path: polygon(0 0, 50% 0, 50% 50%, 100% 50%, 100% 100%, 0 100%);
+
+}  
+</style>
