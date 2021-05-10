fc939e096033bcdd9636e0f0cd0907afcc12a928
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-05-10 13:21:03 -0700

    create dropdown component

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,15 @@
+<template>
+  <vSelect :options="options"></vSelect>
+</template>
+
+<script>
+// import romanNums from '../helpers/basicChordLibrary'
+  export default {
+    name: 'Dropdown',
+    props: [options]
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
