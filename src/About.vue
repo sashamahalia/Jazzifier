b099d7b2c92de152d825d1d95e1fcdc96d4cf23f
Stephanie <stephanzonator@hotmail.com>
2021-05-17 12:01:07 -0700

    fixed info margin, proofreading

--- src/About.vue
+++ src/About.vue
@@ -1,27 +1,27 @@
 <template lang="">
   <div id="app">
     <NavComponent />
-    <h4>About Jazzifier</h4>
+    <h3>About Jazzifier</h3>
     <p class="about-paragraph">Jazzifier is a music theory app that takes simple chord structures and makes them spicier.</p>
 
-    <h4>Ok cool, how do I use it?</h4>
+    <h3>Ok cool, how do I use it?</h3>
 
-    <p class="about-paragraph">First pick a key and mode, we start in the key of C major by default, but you can change it to any major or minor key.
-      Then either pick some chords from the droplist or, if you're feeling indecisive, click "Pick chords for me".
-      You can listen to your basic chords be played at this point, hear them on a syntheizer of your choice, and see them light up on the piano. But these chords aren't spicy! Click the jazzify button to see and hear your chords tranformed into beautiful jazz chords.
-      Once you're done listening, you can stop play and click on any of the chord's names to see just that chord displayed so you can see how to play it yourself. </p>
+    <p class="about-paragraph">First pick a key and mode. We start in the key of C major by default, but you can change it to any major or minor key.
+      Then either pick some chords from the droplist, or if you're feeling indecisive, click "Pick chords for me".
+      You can listen to your basic chords be played at this point, hear them on a synthesizer of your choice, and see them light up on the piano. But these chords aren't spicy! Click the jazzify button to see and hear your chords tranformed into beautiful jazz chords.
+      Once you're done listening, you can stop play and click on any of the chords' names to see just that chord displayed so you can see how to play it yourself. </p>
 
 
-    <h4>What are chords?</h4>
+    <h3>What are chords?</h3>
 
     <p class="about-paragraph">Melodic music uses notes as building blocks to create beautiful songs. When you play one note at a time, that's called a melody. When you play more than one note at a time, these are called chords. Like painters using different colors to create art, musical composers use different chords to make music sound happy, sad, tense, or discordant.</p>
 
     <p class="about-paragraph">When you listen to a lot of music, you might notice that some songs feel similar to each other, even if their melodies are completely different. One reason for this might be that they use similar chord structures. <a href="https://www.youtube.com/watch?v=5pidokakU4I"> Check out the Four Chord Song by Axis of Awesome for an example of what this means.</a> So if you're looking to spice up your chords to stand out from the crowd, you've found the right place!
     </p>
 
-    <h4>What are chord extensions?</h4>
+    <h3>What are chord extensions?</h3>
 
-    <p class="about-paragraph">s you play around with the jazzify button, you'll notice chords have names like 'Fm9' or 'Edim7'. What do those numbers mean? A basic three note chord is a triad.
+    <p class="about-paragraph">As you play around with the jazzify button, you'll notice chords have names like 'Fm9' or 'Edim7'. What do those numbers mean? A basic three note chord is a triad.
       Chord extensions refer to stacking intervals above a triad for more colour and emotion.
       Even though there are only 7 notes in a conventional (diatonic) scale, numbers like 9 or 13 in chords refer to playing notes an octave above the root note of the chord.
       So even though the '9' in a C major 9 chord is a 'D' note which is a '2' in the C major scale, we call it a 9 to show that the 'D' is played an octave higher.
@@ -30,7 +30,7 @@
       If you want know more about musical intervals and how the relate to not only chords but every sound we hear, check out <a href="https://www.youtube.com/watch?v=Wx_kugSemfY&ab_channel=ANDREWHUANG">Andrew Huang's excellent video</a> on the harmonic series.</p>
 
 
-    <h4>We support Egg Eggerson</h4>
+    <h3>We support Egg Eggerson</h3>
   </div>
 </template>
 <script>
@@ -58,7 +58,9 @@ export default {
   }
 
   .about-paragraph {
-    font-size: .75em;
+    margin: 2em auto;
+    max-width: 45em;
+    font-size: 1em;
     font-family: 'PT Sans', sans-serif;
   }
 </style>
