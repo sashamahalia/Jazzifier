4f9709089f87056c299d58b97e51d1b94d601149
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-05-17 15:14:28 -0700

    Add synths to App to avoid audio destruction on disposal

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <NavComponent />
     <div class="leftalign">
       <div class="synth-menu">
-        <h4>Synth type</h4>
+        <h4>Instrument:</h4>
         <v-select :clearable="false" :disabled="disabled" :options="synthTones" v-model="synthTone" name="Instrument" class="instrument"/>
       </div>
     </div>
@@ -34,7 +34,6 @@ import JazzifyButton from './components/JazzifyButton'
 import MenuList from './components/MenuList.vue'
 import ButtonPickChords from './components/ButtonPickChords'
 //helpers
-import { synth, fmSynth, amSynth, sampler } from './helpers/synth'
 import { selectChords } from './helpers/chords'
 import { convertNoteToKeyboard } from './helpers/convertNoteToKeyboard'
 //libraries
@@ -68,8 +67,8 @@ export default {
       scale: {key: 'C', mode: 'Major'},
       beat: this.currentBeat,
       chord: this.currentChord,
-      synthTones: ['basic synth', 'fm wah', 'square', 'piano'],
-      synthTone: 'basic synth',
+      synthTones: ['piano', 'basic synth', 'fm wah', 'square', 'harp'],
+      synthTone: 'piano',
       disabled: false
 
     }
@@ -89,16 +88,27 @@ export default {
     },
 
     chordLoop() {
+
+      // Get rid of preexisting instruments
+      this.piano().dispose();
+      this.synth().dispose();
+      this.fmSynth().dispose();
+      this.amSynth().dispose();
+      this.harp().dispose();
+
+
       let synthTone = ''
 
-      if (this.synthTone === 'basic synth') {
-        synthTone = synth;
+      if (this.synthTone === 'piano') {
+        synthTone = this.piano();
+      } else if (this.synthTone === 'basic synth') {
+        synthTone = this.synth();
       } else if (this.synthTone === 'fm wah') {
-        synthTone = fmSynth;
+        synthTone = this.fmSynth();
       } else if (this.synthTone === 'square') {
-        synthTone = amSynth;
-      } else if (this.synthTone === 'piano') {
-        synthTone = sampler;
+        synthTone = this.amSynth();
+      } else if (this.synthTone === 'harp') {
+        synthTone = this.harp();
       }
 
       const loop = new Tone.Part((time, value) => {
@@ -218,7 +228,98 @@ export default {
     onChordToggle(position, index) {
       this.beat = position.beat;
       return this.chord = this.selectChords[index];
+    },
+    piano() {
+      // Multisampled piano, thanks to https://github.com/ledlamp/piano-sounds
+      return new Tone.Sampler({
+        volume: -10,
+        urls: {
+          C1: 'c1.mp3',
+          C2: 'c2.mp3',
+          C3: 'c3.mp3',
+          D3: 'd3.mp3',
+          E3: 'e3.mp3',
+          F3: 'f3.mp3',
+          G3: 'g3.mp3',
+          A3: 'a3.mp3',
+          B3: 'b3.mp3',
+          C4: 'c4.mp3',
+          D4: 'd4.mp3',
+          E4: 'e4.mp3',
+          F4: 'f4.mp3',
+          G4: 'g4.mp3',
+          A4: 'a4.mp3',
+          B4: 'b4.mp3'
+        },
+        baseUrl: 'https://ledlamp.github.io/piano-sounds/GreatAndSoftPiano/',
+      }).toDestination();
+    },
+    synth() {
+      return  new Tone.PolySynth(Tone.Synth, {
+        oscillator : {
+        type : "sawtooth",
+        detune: 3,
+        portamento: 0.5,
+        volume: -15
+        }
+      }).toDestination();
+    },
+      fmSynth() {
+        const fmFilter = new Tone.AutoFilter({
+        frequency: 30,
+        depth: 0,
+        baseFrequency: 200
+      });
+
+      return new Tone.PolySynth(Tone.FMSynth, {
+        volume: -10,
+        harmonicity: 1,
+        modulationIndex: 10,
+        oscillator : {
+            type: "sine"
+      }}).connect(fmFilter).toDestination();
+    },
+    amSynth() {
+          const amFilter = new Tone.AutoFilter({
+        frequency: 30,
+        depth: 0,
+        baseFrequency: 200
+      });
+
+      return new Tone.PolySynth(Tone.AMSynth, {
+        harmonicity: 1,
+        modulationIndex: 5,
+        portamento: 2,
+        volume: -15,
+        detune: 3,
+        oscillator : {
+            type: "square"
+      }}).connect(amFilter).toDestination();
+    },
+    harp() {
+      return new Tone.Sampler({
+        urls: {
+          C1: 'c0.mp3',
+          C2: 'c1.mp3',
+          C3: 'c2.mp3',
+          D3: 'd2.mp3',
+          E3: 'e2.mp3',
+          F3: 'f2.mp3',
+          G3: 'g2.mp3',
+          A3: 'a2.mp3',
+          B3: 'b2.mp3',
+          C4: 'c3.mp3',
+          D4: 'd3.mp3',
+          E4: 'e3.mp3',
+          F4: 'f3.mp3',
+          G4: 'g3.mp3',
+          A4: 'a3.mp3',
+          B4: 'b3.mp3'
+        },
+        baseUrl: 'https://ledlamp.github.io/piano-sounds/Harp/',
+      }).toDestination();
     }
+    
   }
 }
 </script>
--- src/helpers/synth.js
+++ src/helpers/synth.js
@@ -1,64 +1,87 @@
-import * as Tone from 'tone';
+// import * as Tone from 'tone';
 
-export let synth = new Tone.PolySynth(Tone.Synth, {
-  oscillator : {
-    type : "sawtooth",
-    detune: 3,
-    portamento: 0.5,
-    volume: -15
-  }
-}).toDestination();
+// export let synth = new Tone.PolySynth(Tone.Synth, {
+//   oscillator : {
+//     type : "sawtooth",
+//     detune: 3,
+//     portamento: 0.5,
+//     volume: -15
+//   }
+// }).toDestination();
 
-const fmFilter = new Tone.AutoFilter({
-  frequency: 30,
-  depth: 0,
-  baseFrequency: 200
-});
+// const fmFilter = new Tone.AutoFilter({
+//   frequency: 30,
+//   depth: 0,
+//   baseFrequency: 200
+// });
 
-export const fmSynth = new Tone.PolySynth(Tone.FMSynth, {
-  volume: -10,
-  harmonicity: 1,
-  modulationIndex: 10,
-  oscillator : {
-      type: "sine"
-  }}).connect(fmFilter).toDestination();
+// export const fmSynth = new Tone.PolySynth(Tone.FMSynth, {
+//   volume: -10,
+//   harmonicity: 1,
+//   modulationIndex: 10,
+//   oscillator : {
+//       type: "sine"
+//   }}).connect(fmFilter).toDestination();
 
-  const amFilter = new Tone.AutoFilter({
-    frequency: 30,
-    depth: 0,
-    baseFrequency: 200
-  });
+//   const amFilter = new Tone.AutoFilter({
+//     frequency: 30,
+//     depth: 0,
+//     baseFrequency: 200
+//   });
 
-  export const amSynth = new Tone.PolySynth(Tone.AMSynth, {
-    harmonicity: 1,
-    modulationIndex: 5,
-    portamento: 2,
-    volume: -15,
-    detune: 3,
-    oscillator : {
-        type: "square"
-  }}).connect(amFilter).toDestination();
+//   export const amSynth = new Tone.PolySynth(Tone.AMSynth, {
+//     harmonicity: 1,
+//     modulationIndex: 5,
+//     portamento: 2,
+//     volume: -15,
+//     detune: 3,
+//     oscillator : {
+//         type: "square"
+//   }}).connect(amFilter).toDestination();
 
 
-  // Multisampled piano, thanks to https://github.com/ledlamp/piano-sounds
-  export const sampler = new Tone.Sampler({
-    urls: {
-      C1: 'c0.mp3',
-      C2: 'c1.mp3',
-      C3: 'c2.mp3',
-      D3: 'd2.mp3',
-      E3: 'e2.mp3',
-      F3: 'f2.mp3',
-      G3: 'g2.mp3',
-      A3: 'a2.mp3',
-      B3: 'b2.mp3',
-      C4: 'c3.mp3',
-      D4: 'd3.mp3',
-      E4: 'e3.mp3',
-      F4: 'f3.mp3',
-      G4: 'g3.mp3',
-      A4: 'a3.mp3',
-      B4: 'b3.mp3'
-    },
-    baseUrl: 'https://ledlamp.github.io/piano-sounds/GreatAndSoftPiano/',
-  }).toDestination();
+//   // Multisampled piano, thanks to https://github.com/ledlamp/piano-sounds
+//   export const piano = new Tone.Sampler({
+//     volume: -10,
+//     urls: {
+//       C1: 'c1.mp3',
+//       C2: 'c2.mp3',
+//       C3: 'c3.mp3',
+//       D3: 'd3.mp3',
+//       E3: 'e3.mp3',
+//       F3: 'f3.mp3',
+//       G3: 'g3.mp3',
+//       A3: 'a3.mp3',
+//       B3: 'b3.mp3',
+//       C4: 'c4.mp3',
+//       D4: 'd4.mp3',
+//       E4: 'e4.mp3',
+//       F4: 'f4.mp3',
+//       G4: 'g4.mp3',
+//       A4: 'a4.mp3',
+//       B4: 'b4.mp3'
+//     },
+//     baseUrl: 'https://ledlamp.github.io/piano-sounds/GreatAndSoftPiano/',
+//   }).toDestination();
+
+//   export const harp = new Tone.Sampler({
+//     urls: {
+//       C1: 'c0.mp3',
+//       C2: 'c1.mp3',
+//       C3: 'c2.mp3',
+//       D3: 'd2.mp3',
+//       E3: 'e2.mp3',
+//       F3: 'f2.mp3',
+//       G3: 'g2.mp3',
+//       A3: 'a2.mp3',
+//       B3: 'b2.mp3',
+//       C4: 'c3.mp3',
+//       D4: 'd3.mp3',
+//       E4: 'e3.mp3',
+//       F4: 'f3.mp3',
+//       G4: 'g3.mp3',
+//       A4: 'a3.mp3',
+//       B4: 'b3.mp3'
+//     },
+//     baseUrl: 'https://ledlamp.github.io/piano-sounds/Harp/',
+//   }).toDestination();
