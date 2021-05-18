3df53e96e7cf2a8660d5e27c8dd7c2811596f01e
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-05-17 18:05:45 -0700

    Add click animation to jazzify button

--- src/components/JazzifyButton.vue
+++ src/components/JazzifyButton.vue
@@ -1,32 +1,51 @@
 <template>
   <div>
-    <button id="jazzify" @click="onClickJazzify" :disabled="disabled" >
+    <button id="jazzify" :class="{'jazzify': animated}" @animationend="animated = false" @click="onClickJazzify" :disabled="disabled" >
       &#127927; Jazzify! &#127927;
     </button>
-    <DancingNotes />
   </div>
 </template>
 
 
 <script>
-import DancingNotes from './DancingNotes'
 export default {
-name: 'JazzifyButton',
-  props: ['disabled' ],
   name: 'JazzifyButton',
-  components: {
-    DancingNotes
+  data() {
+    return {
+      animated: false
+    }
   },
+  props: ['disabled' ],
   methods: {
     onClickJazzify() {
       
       // console.log("jazzify clicked")
       this.$emit('jazzifyClicked')
+      this.animate();
+    },
+    animate() {
+      this.animated = true;
     }
   }
 }
 </script>
 
 
-<style lang="">  
+<style lang="scss">
+
+.jazzify {
+  animation: jazzify 1.5s ease-in-out both 1;
+  animation-timing-function: linear;
+}
+
+@keyframes jazzify {
+  0% {background-color: $darkest-purple; transform: scale(0.9)}
+  20% {background-color: $medium-purple; }
+  40% {background-color: $pink; }
+  60% {background-color: $light-pink; }
+  80% {background-color: $medium-purple;}
+  100% {background-color: $darkest-purple; }
+  
+}
+
 </style>
