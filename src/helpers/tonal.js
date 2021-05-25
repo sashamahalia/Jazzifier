01e18279ea03f1f486f96b6e04e7bc6e7e43c7a4
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-05-25 09:34:16 -0700

    sort chords by major and minor based on chords quality

--- src/helpers/tonal.js
+++ src/helpers/tonal.js
@@ -1,22 +1,25 @@
 import { ChordType } from "@tonaljs/tonal";
 
 export const getChords = () => {
-  const funChords = ChordType.all().filter(chord => chord.intervals.length > 3 && chord.intervals.length < 6);
-  // returns the first of the abrivations for the chord as a string
-  return funChords.map(chord => chord.aliases[0]);
+  const funChords = ChordType.all().filter(chord => (chord.intervals.length > 3 && chord.intervals.length < 6) && (!chord.aliases[0].includes('11') && (!chord.aliases[0].includes('11'))));
+  return funChords;
 }
-console.log('chords objects', ChordType.all().filter(chord => chord.intervals.length > 3 && chord.intervals.length < 6));
-// console.log('get chords', getChords());
+console.log('get chords', getChords());
 
-// // gets all extensions that start with a minor symbol or dim
-// const minorExtensions = getChords().filter(chord => (chord.charAt(0) === 'm' && !chord.startsWith('maj')) || chord.startsWith('dim'));
-// console.log('minor', minorExtensions);
+// gets all extensions that are minor, diminished, or suspended
+const minorExtensions = getChords().filter(chord => (chord.quality !== 'Major') && (chord.quality !== 'Augmented'));
+const minorStrings = minorExtensions.map(chord => chord.aliases[0]);
+  // returns the first of the abrivations for the chord as a string
+console.log('minor', minorStrings);
 
-// // gets all other extensions
-// const majorExtensions = getChords().filter(chord => !minorExtensions.includes(chord));
-// console.log('major', majorExtensions);
+// funChords.map(chord => chord.aliases[0]);
 
-// // Refactor to use randomChord method in App, by making the chord in this.chords[randomNumber] a variable based on method argument.
+// gets all chords that are major, augmented or suspended.
+const majorExtensions = getChords().filter(chord => (chord.quality !== 'Minor') && (chord.quality !== 'Diminished'));
+const majorStrings = majorExtensions.map(chord => chord.aliases[0]);
+
+console.log('major', majorStrings);
+
 // const randomJazzChord = (chordArray) => {
 //   const min = Math.ceil(0);
 //   const max = Math.floor(chordArray.length - 1);
@@ -28,10 +31,10 @@ console.log('chords objects', ChordType.all().filter(chord => chord.intervals.le
 // // Builds arrays of all compatable chords based on chord mode, and outputs a random chord extension.
 // const chordChange = (chordName, chordMode) => {
 //   if (chordMode === 'Major') {
-//     return `${chordName}${randomJazzChord(majorExtensions)}`
+//     return `${chordName}${randomJazzChord(majorStrings)}`
 //   }
 
 //   if (chordMode === 'Minor') {
-//     return `${chordName}${randomJazzChord(minorExtensions)}`
+//     return `${chordName}${randomJazzChord(minorStrings)}`
 //   }
 // };
