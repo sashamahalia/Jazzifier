7382bee84fbfbd44bb0e8d045b8a778fcf69e16f
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-06-08 12:03:07 -0700

    Add appropriate headings and landmarks for About page

--- src/About.vue
+++ src/About.vue
@@ -3,10 +3,11 @@
     <NavComponent />
     <br>
     <br>
-    <h3 class="heading">About Jazzifier</h3>
+    <main>
+    <h1 class="heading">About Jazzifier</h1>
     <p class="about-paragraph">Jazzifier is a music theory app that takes simple chord structures and makes them spicier.</p>
 
-    <h3>Ok cool, how do I use it?</h3>
+    <h2>Ok cool, how do I use it?</h2>
 
     <p class="about-paragraph">First pick a key and mode. We start in the key of C major by default, but you can change it to any major or minor key.
       Then either pick some chords from the droplist, or if you're feeling indecisive, click "Pick chords for me".
@@ -14,14 +15,14 @@
       Once you're done listening, you can stop play and click on any of the chords' names to see just that chord displayed so you can see how to play it yourself. </p>
 
 
-    <h3>What are chords?</h3>
+    <h2>What are chords?</h2>
 
     <p class="about-paragraph">Melodic music uses notes as building blocks to create beautiful songs. When you play one note at a time, that's called a melody. When you play more than one note at a time, these are called chords. Like painters using different colors to create art, musical composers use different chords to make music sound happy, sad, tense, or discordant.</p>
 
     <p class="about-paragraph">When you listen to a lot of music, you might notice that some songs feel similar to each other, even if their melodies are completely different. One reason for this might be that they use similar chord structures. <a href="https://www.youtube.com/watch?v=5pidokakU4I"> Check out the Four Chord Song by Axis of Awesome for an example of what this means.</a> So if you're looking to spice up your chords to stand out from the crowd, you've found the right place!
     </p>
 
-    <h3>What are chord extensions?</h3>
+    <h2>What are chord extensions?</h2>
 
     <p class="about-paragraph">As you play around with the jazzify button, you'll notice chords have names like 'Fm9' or 'Edim7'. What do those numbers mean? A basic three note chord is a triad.
       Chord extensions refer to stacking intervals above a triad for more colour and emotion.
@@ -31,10 +32,9 @@
       
       If you want know more about musical intervals and how the relate to not only chords but every sound we hear, check out <a href="https://www.youtube.com/watch?v=Wx_kugSemfY&ab_channel=ANDREWHUANG">Andrew Huang's excellent video</a> on the harmonic series.</p>
 
-
-    <h3>We support Egg Eggerson</h3>
     <br>
     <br>
+    </main>
   </div>
 </template>
 <script>
