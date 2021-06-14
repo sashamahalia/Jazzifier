688c93393d2b8cff4a847e1fca96f7ab608c26a3
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-06-14 14:54:12 -0700

    Test nav bar navigation, delete example tests

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,11 +1,13 @@
 describe("Navigation", () => {
   it("should visit root", () => {
-    cy.visit("/");
+    cy.visit("http://localhost:8082/");
   });
-  it("should navigate to the about page", () => {
-    cy.visit("/about");
-    // cy.contains("[data-testid=day]", "Tuesday")
-    // .click()
-    // .should("have.class", "day-list__item--selected")
+  it("should navigate to the about page and back to home page with nav bar", () => {
+    cy.get('.navlink').contains('Info')
+      .click();
+    cy.contains('About Jazzifier');
+    cy.get('.navlink').contains('Jazzifier')
+      .click();
+      cy.contains('Instrument');
   });
 });
